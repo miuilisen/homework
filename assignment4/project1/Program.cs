a090f950704df1c68e00a9e6dd450c5c07b86a56
miuilisen <92162603+miuilisen@users.noreply.github.com>
2023-06-08 22:49:09 +0800

    commit

--- assignment4/project1/Program.cs
+++ assignment4/project1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 public class Node<T>
 {
     public Node<T> Next { get; set; }
@@ -15,6 +17,7 @@ public class GenericList<T>
     private Node<T> tail;
     public GenericList()
     {
+
         tail = head = null;
     }
     public Node<T> Head
@@ -33,4 +36,47 @@ public class GenericList<T>
             tail = n;
         }
 }
+    //添加Foreach方法
+    public void ForEach(Action<T> action)
+    {
+        Node<T> tem = head;
+        while (tem != null)
+        {
+            action(tem.Data);
+            tem = tem.Next;
+        }
+    }
+
+}
+public class program
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> list1 = new GenericList<int>();
+        list1.Add(1);    
+        list1.Add(2);
+        list1.Add(3);
+        list1.Add(4);
+        list1.Add(5);
+        list1.ForEach(data => Console.WriteLine(data));
+
+        int max=int.MinValue ;
+        int min=int.MaxValue;
+        int sum = 0;
+
+        list1.ForEach(data =>
+        {
+            if (data > max)
+                max = data;
+
+            if (data < min)
+                min = data;
+
+            sum += data;
+        });
+
+        Console.WriteLine("最大值: " + max);
+        Console.WriteLine("最小值: " + min);
+        Console.WriteLine("Sum: " + sum);
+    }
 }
--- assignment4/project2/Program.cs
+++ assignment4/project2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Clock
+{
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    public void Start()
+    {
+        while (true)
+        {
+        Thread.Sleep(1000); 
+            // 检查是否需要触发响铃事件
+            if (DateTime.Now.Hour == 21 && DateTime.Now.Minute == 30)          
+                OnAlarm(EventArgs.Empty);
+            else
+                // 触发嘀嗒事件
+                Ontick(EventArgs.Empty);
+        }
+    }
+
+     void Ontick(EventArgs e)
+    {
+       if (Tick != null)
+                Tick(this, e);
+    }
+
+    void OnAlarm(EventArgs e)
+    {
+        if (Alarm != null)
+            Alarm(this, e);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clock clock = new Clock();
+
+        // 注册嘀嗒事件的处理方法
+        clock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("Dida.");
+        };
+
+        // 注册响铃事件的处理方法
+        clock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("Alarm!");
+        };
+        clock.Start();
+    }
+}
